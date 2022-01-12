12ec138ad4f64b95fdd305094f30b473b5a91613
dinhph18273 <hobao3210@gmail.com>
2022-01-12 10:18:47 +0700

    update-lab3

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -4,9 +4,9 @@ const DetailPage = {
     print(id) {
         const result = data.find((post) => post.id === id);
         return /* html */ `
-            <div>   
-                <h1>${result.title}</h1>
-                <img src="${result.img}" alt="" />
+            <div class = "border border-black max-w-sm mt-2 mx-auto text-center p-5 rounded">                  
+                <img class = "max-w-xs" src="${result.img}" alt="" />
+                <h1 class = "text-2xl mb-2 text-orange-500">${result.title}</h1>
                 <p>${result.desc}</p>
             </div>
         `;
