af1907973ae36c39c36d9875c7e1e03ad3b4b8f1
dinhph18273 <hobao3210@gmail.com>
2022-01-15 09:28:19 +0700

    update-lab3

--- src/pages/admin/productEdit.js
+++ src/pages/admin/productEdit.js
@@ -1,10 +1,11 @@
+import NavBarDas from "../../components/Nav";
 import { data } from "../../data";
 
 const ProductEdit = {
     print(id) {
         const result = data.find((post) => post.id === id);
         return /* html */ `
-        
+        ${NavBarDas.print()}
         <form class = "text-center mt-2">
             <input type="text" class="border mt-2" value="${result.title}" placeholder="Tiêu đề bài viết" id="post-title"/> <br/>
             <img class = "max-w-xs mx-auto mt-2" src="${result.img}" alt="" />
@@ -12,7 +13,7 @@ const ProductEdit = {
             <textarea name="" class="border mt-2"  id="" cols="30" rows="10" id="post-desc">${result.desc}</textarea><br/>
             <button class="btn border px-5 py-2 mt-2 bg-sky-500 text-white">Cập nhật</button>
         </form>
-        <a href = "/admin/news">Back</a>
+        <a href = "/admin/news" class = "text-red-500">Back</a>
         `;
     },
 };
