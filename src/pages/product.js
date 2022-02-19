4f400cd7ae3825950e1aa38757f48f50f6092e0c
dinhph18273 <hobao3210@gmail.com>
2022-02-19 12:08:06 +0700

    update

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,43 +1,54 @@
+import Banner from "../components/banner";
+import category from "../components/cate";
+import commit from "../components/commitment";
 import footer from "../components/footer";
 import header from "../components/header";
 import headerTop from "../components/headerTop";
-// eslint-disable-next-line import/no-cycle
-import listCaTe from "../components/listCaTe";
+import NewsList from "../components/newsList";
 import products from "../components/product";
 
-const ProductPage = {
-    async print(arr) {
+const Homepage = {
+    async print() {
         return /* html */ `
-        
-        <header class = "shadow-md">
-         <div class="max-w-7xl mx-auto">
-            <div id = "header">
-            ${headerTop.print()}
-            </div>
-            <div class="">
-                ${header.print()}
-            </div>
-         </div>
-        </header>
-         <div class="max-w-7xl mx-auto mb-10">
-            <h1 class = "text-2xl my-7"><span class = "opacity-70">Trang chủ</span> / Cửa hàng</h1>
-            <div class = "flex">
-                <div class = "">    
-                ${listCaTe.print()}
-                </div>
-                <div class = "grid grid-cols-4 gap-8" id="product">
-                    ${await products.print(arr)}
-                </div>
-            </div>
+        <div class="max-w-7xl mx-auto ">
+                <header class = "">
+                    <div id = "header">
+                    ${headerTop.print()}
+                    </div>
+                    <div class="mt-5">
+                        ${header.print()}   
+                    </div>  
+                </header>         
         </div>
-        ${footer.print()}
+        <div class="banner relative">
+            ${Banner.print()}
+        </div>5
+        <div class="max-w-7xl mx-auto">
+                <h1 class = "text-3xl capitalize text-center my-10 normal">Mua sản phẩm được lựa chọn từ vườn</h1>
+                ${category.print()}
+                <h1 class = "text-3xl capitalize text-center my-10 normal">Sản phẩm nổi bật</h1>
+                    <div class = "grid grid-cols-6 gap-8">
+                        ${await products.print()}
+                    </div>
+                    <div class = "text-center my-10"> 
+                    <a href = "/#/products" class = "py-2 px-5 text-center bg-lime-500 text-white rounded-md hover:bg-lime-600">Xem thêm</a>
+                    </div>
+            <main>
+                <div class="news mt-10">
+                        ${await NewsList.print()}
+                </div>
+            </main>
+                <h1 class = "text-3xl capitalize text-center my-10 normal">Cam kết của chúng tôi</h1>
+                ${commit.print()}
+        </div>
+                ${footer.print()}
         `;
     },
     async afterRender() {
         headerTop.afterRender();
-        await listCaTe.afterRender();
         products.afterRender();
         await header.afterRender();
     },
 };
-export default ProductPage;
+
+export default Homepage;
